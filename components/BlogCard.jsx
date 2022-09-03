48623fe5a09094dbcfdd56b00d0c7f3f070c9d1c
Gautam Singla <ggsingla777@gmail.com>
2022-09-03 21:17:52 +0530

    bug fixes

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -4,43 +4,45 @@ const BlogCard = () => {
   return (
     <>
       <a
-        class='relative block max-w-sm p-8 overflow-hidden border border-gray-100 rounded-lg'
+        className='relative block max-w-sm p-8 overflow-hidden border border-gray-100 rounded-lg'
         href=''>
-        <span class='absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'></span>
+        <span className='absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'></span>
 
-        <div class='justify-between sm:flex'>
+        <div className='justify-between sm:flex'>
           <div>
-            <h5 class='text-xl font-bold text-gray-900'>
+            <h5 className='text-xl font-bold text-gray-900'>
               Building a SaaS product as a software developer
             </h5>
-            <p class='mt-1 text-xs font-medium text-gray-600'>By John Doe</p>
+            <p className='mt-1 text-xs font-medium text-gray-600'>
+              By John Doe
+            </p>
           </div>
 
-          <div class='flex-shrink-0 hidden ml-3 sm:block'>
+          <div className='flex-shrink-0 hidden ml-3 sm:block'>
             <img
-              class='object-cover w-16 h-16 rounded-lg shadow-sm'
+              className='object-cover w-16 h-16 rounded-lg shadow-sm'
               src='https://www.hyperui.dev/photos/man-5.jpeg'
               alt=''
             />
           </div>
         </div>
 
-        <div class='mt-4 sm:pr-8'>
-          <p class='text-sm text-gray-500'>
+        <div className='mt-4 sm:pr-8'>
+          <p className='text-sm text-gray-500'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit
             illum provident a, ipsa maiores deleniti consectetur nobis et eaque.
           </p>
         </div>
 
-        <dl class='flex mt-6'>
-          <div class='flex flex-col-reverse'>
-            <dt class='text-sm font-medium text-gray-600'>Published</dt>
-            <dd class='text-xs text-gray-500'>31st June, 2021</dd>
+        <dl className='flex mt-6'>
+          <div className='flex flex-col-reverse'>
+            <dt className='text-sm font-medium text-gray-600'>Published</dt>
+            <dd className='text-xs text-gray-500'>31st June, 2021</dd>
           </div>
 
-          <div class='flex flex-col-reverse ml-3 sm:ml-6'>
-            <dt class='text-sm font-medium text-gray-600'>Reading time</dt>
-            <dd class='text-xs text-gray-500'>3 minute</dd>
+          <div className='flex flex-col-reverse ml-3 sm:ml-6'>
+            <dt className='text-sm font-medium text-gray-600'>Reading time</dt>
+            <dd className='text-xs text-gray-500'>3 minute</dd>
           </div>
         </dl>
       </a>
